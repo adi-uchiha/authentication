0984fd07192a4544355c259375312e78a18eab36
Aditya Shelke <adi.cs.night@gmail.com>
2022-08-26 03:18:15 +0530

    Got the api/users endpoint working

---
+++ pages/api/users/index.js
@@ -0,0 +1,36 @@
+import dbConnect from '../../../lib/dbConnect'
+import user from '../../../models/User'
+
+console.log("called users")
+export default async function handler(req, res) {
+    
+  const { method } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const pets = await user.find({}) /* find all the data in our database */
+        res.status(200).json({ success: true, data: pets })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case 'POST':
+      try {
+        console.log("heloooooooooooooooooooooooo")
+        const User = await user.create(
+          req.body
+        ) /* create a new model in the database */
+        res.status(201).json({ success: true, data: User })
+      } catch (error) {
+        console.log(error)
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
